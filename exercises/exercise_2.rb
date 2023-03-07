c65bac157ef1dbe7de8a66dcc5427eb029e339ef
RJ <randy.j416@yahoo.com>
2023-03-06 21:15:40 -0500

    do exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+puts @store1.inspect
+@store2 = Store.find_by(id: 2)
+@store1.name = "Surrey"
+@store1.save
+# alternatively
+# @store1.update(name: "Surry")
