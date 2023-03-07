a31840909df5c1b7fc2ae333ea692bdaba1c0111
RJ <randy.j416@yahoo.com>
2023-03-06 21:35:31 -0500

    do exercise 4, renamed a store to avoid duplicate names

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -9,7 +9,7 @@ puts "----------"
 @store1 = Store.find_by(id: 1)
 puts @store1.inspect
 @store2 = Store.find_by(id: 2)
-@store1.name = "Surrey"
+@store1.name = "South Surrey"
 @store1.save
 # alternatively
-# @store1.update(name: "Surry")
+# @store1.update(name: "South Surrey")
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -8,4 +8,4 @@ puts "----------"
 # Your code goes here ...
 @store3 = Store.find_by(id: 3)
 @store3.destroy
-puts stores.count
+puts Store.count
